4ad0525c3ad0a7362eeadd43827ad78f9fa25124
gsdriver <gsdriver@live.com>
2017-02-06 07:21:15 -0800

    Add more test cases

--- src/StubbedGame.js
+++ src/StubbedGame.js
@@ -47,6 +47,63 @@ const dealerAceState = [
                   "resplitAces":false,"blackjackBonus":0.5,"numberOfDecks":1,"minBet":5,"maxBet":1000,"maxSplitHands":4}}
     ];
 
+// Flow - deal a 16 against a dealer 10, and surrender
+const surrenderState = [
+  // First deal the hand with 16 and a 10 showing
+  {"userID":"stubbed","activePlayer":"player","currentPlayerHand":0,"bankroll":4780,
+  "possibleActions":["hit","stand","surrender","double"],"dealerHand":{"outcome":"playing","cards":[{"rank":0,"suit":"none"},
+  {"rank":12,"suit":"diamonds"}],"total":10,"soft":false},"playerHands":[{"bet":20,"busted":false,
+  "cards":[{"rank":13,"suit":"clubs"},{"rank":6,"suit":"hearts"}],"outcome":"playing","total":16,"soft":false}],"lastBet":"20",
+  "houseRules":{"hitSoft17":false,"surrender":"late","double":"any","doubleaftersplit":true,
+                "resplitAces":false,"blackjackBonus":0.5,"numberOfDecks":1,"minBet":5,"maxBet":1000,"maxSplitHands":4}},
+  // Then surrender
+  {"userID":"stubbed","activePlayer":"none","currentPlayerHand":0,"bankroll":4790,
+  "possibleActions":["bet"],"dealerHand":{"outcome":"playing","cards":[{"rank":12,"suit":"hearts"},{"rank":12,"suit":"diamonds"}],
+  "total":20,"soft":false},"playerHands":[{"bet":20,"busted":false,"cards":[{"rank":13,"suit":"clubs"},{"rank":6,"suit":"hearts"}],
+  "outcome":"surrender","total":16,"soft":false}],"lastBet":"20",
+  "houseRules":{"hitSoft17":false,"surrender":"late","double":"any","doubleaftersplit":true,"resplitAces":false,
+                "blackjackBonus":0.5,"numberOfDecks":1,"minBet":5,"maxBet":1000,"maxSplitHands":4}}
+];
+
+// Flow - several hits before standing
+const hittingStreakState = [
+  // Deal a soft 13 against dealer 10
+  {"userID":"stubbed","activePlayer":"player","currentPlayerHand":0,"bankroll":4760,
+  "possibleActions":["hit","stand","surrender","double"],"dealerHand":{"outcome":"playing","cards":[{"rank":0,"suit":"none"},
+  {"rank":12,"suit":"spades"}],"total":10,"soft":false},"playerHands":[{"bet":30,"busted":false,"cards":[{"rank":2,"suit":"clubs"},
+  {"rank":1,"suit":"spades"}],"outcome":"playing","total":13,"soft":true}],"lastBet":"30",
+  "houseRules":{"hitSoft17":false,"surrender":"late","double":"any","doubleaftersplit":true,
+                "resplitAces":false,"blackjackBonus":0.5,"numberOfDecks":1,"minBet":5,"maxBet":1000,"maxSplitHands":4}},
+  // Draw a 3
+  {"userID":"stubbed","activePlayer":"player","currentPlayerHand":0,"bankroll":4760,
+  "possibleActions":["hit","stand"],"dealerHand":{"outcome":"playing","cards":[{"rank":0,"suit":"none"},{"rank":12,"suit":"spades"}],
+  "total":10,"soft":false},"playerHands":[{"bet":30,"busted":false,"cards":[{"rank":2,"suit":"clubs"},{"rank":1,"suit":"spades"},
+  {"rank":3,"suit":"hearts"}],"outcome":"playing","total":16,"soft":true}],"lastBet":"30",
+  "houseRules":{"hitSoft17":false,"surrender":"late","double":"any","doubleaftersplit":true,
+                "resplitAces":false,"blackjackBonus":0.5,"numberOfDecks":1,"minBet":5,"maxBet":1000,"maxSplitHands":4}},
+  // Draw a 10
+  {"userID":"stubbed","activePlayer":"player","currentPlayerHand":0,"bankroll":4760,
+  "possibleActions":["hit","stand"],"dealerHand":{"outcome":"playing","cards":[{"rank":0,"suit":"none"},{"rank":12,"suit":"spades"}],
+  "total":10,"soft":false},"playerHands":[{"bet":30,"busted":false,"cards":[{"rank":2,"suit":"clubs"},{"rank":1,"suit":"spades"},
+  {"rank":3,"suit":"hearts"},{"rank":10,"suit":"diamonds"}],"outcome":"playing","total":16,"soft":false}],"lastBet":"30",
+  "houseRules":{"hitSoft17":false,"surrender":"late","double":"any","doubleaftersplit":true,
+                "resplitAces":false,"blackjackBonus":0.5,"numberOfDecks":1,"minBet":5,"maxBet":1000,"maxSplitHands":4}},
+  // Draw a 4
+  {"userID":"stubbed","activePlayer":"player","currentPlayerHand":0,"bankroll":4760,"possibleActions":["hit","stand"],
+  "dealerHand":{"outcome":"playing","cards":[{"rank":0,"suit":"none"},{"rank":12,"suit":"spades"}],"total":10,"soft":false},
+  "playerHands":[{"bet":30,"busted":false,"cards":[{"rank":2,"suit":"clubs"},{"rank":1,"suit":"spades"},{"rank":3,"suit":"hearts"},
+  {"rank":10,"suit":"diamonds"},{"rank":4,"suit":"diamonds"}],"outcome":"playing","total":20,"soft":false}],"lastBet":"30",
+  "houseRules":{"hitSoft17":false,"surrender":"late","double":"any","doubleaftersplit":true,
+                "resplitAces":false,"blackjackBonus":0.5,"numberOfDecks":1,"minBet":5,"maxBet":1000,"maxSplitHands":4}},
+  // Stand
+  {"userID":"stubbed","activePlayer":"none","currentPlayerHand":0,"bankroll":4790,"possibleActions":["bet"],
+  "dealerHand":{"outcome":"playing","cards":[{"rank":13,"suit":"spades"},{"rank":12,"suit":"spades"}],"total":20,"soft":false},
+  "playerHands":[{"bet":30,"busted":false,"cards":[{"rank":2,"suit":"clubs"},{"rank":1,"suit":"spades"},{"rank":3,"suit":"hearts"},
+  {"rank":10,"suit":"diamonds"},{"rank":4,"suit":"diamonds"}],"outcome":"push","total":20,"soft":false}],"lastBet":"30",
+  "houseRules":{"hitSoft17":false,"surrender":"late","double":"any","doubleaftersplit":true,
+                "resplitAces":false,"blackjackBonus":0.5,"numberOfDecks":1,"minBet":5,"maxBet":1000,"maxSplitHands":4}}
+];
+
 var lastGameFlow = null;
 var lastGameFlowIndex = 0;
 
@@ -109,6 +166,14 @@ function DealStubbedHand(value)
         // 10 - dealerAceState flow
         case 10:
             lastGameFlow = dealerAceState;
+            break;
+        // 20 - surrenderState flow
+        case 20:
+            lastGameFlow = surrenderState;
+            break;
+        // 30 - hittingStreak flow
+        case 30:
+            lastGameFlow = hittingStreakState;
             break;
     }
 
--- test/test.js
+++ test/test.js
@@ -25,3 +25,32 @@ conversation(opts)
     .plainResponse
       .shouldEqual("The dealer had a blackjack. ")
   .end();
+
+opts.name = 'Surrender';
+conversation(opts)
+  .userSays('BettingIntent', {Amount: 20})
+    .plainResponse
+      .shouldEqual('You have king and six for a total of 16. The dealer has a queen showing. Would you like to Hit, or Stand, or Surrender, or Double Down?')
+  .userSays('BlackjackIntent', {Action: 'surrender'})
+    .plainResponse
+      .shouldEqual('You surrendered. The dealer had a queen down. The dealer had a total of 20. You have 4790 dollars. Say Deal to play again betting 20 dollars or Bet and the amount you would like to bet.')
+  .end();
+
+opts.name = 'Hit to Tie';
+conversation(opts)
+  .userSays('BettingIntent', {Amount: 30})
+    .plainResponse
+      .shouldEqual('You have two and ace for a total of soft 13. The dealer has a queen showing. Would you like to Hit, or Stand, or Surrender, or Double Down?')
+  .userSays('BlackjackIntent', {Action: 'hit'})
+    .plainResponse
+      .shouldEqual('You got a three for a total of soft 16. The dealer is showing a queen. Would you like to Hit, or Stand?')
+  .userSays('BlackjackIntent', {Action: 'hit'})
+    .plainResponse
+      .shouldEqual('You got a ten for a total of 16. The dealer is showing a queen. Would you like to Hit, or Stand?')
+  .userSays('BlackjackIntent', {Action: 'hit'})
+    .plainResponse
+      .shouldEqual('You got a four for a total of 20. The dealer is showing a queen. Would you like to Hit, or Stand?')
+  .userSays('BlackjackIntent', {Action: 'stand'})
+    .plainResponse
+      .shouldEqual('The dealer had a king down. The dealer had a total of 20. It\'s a tie.  You have 4790 dollars. Say Deal to play again betting 30 dollars or Bet and the amount you would like to bet.')
+  .end();
