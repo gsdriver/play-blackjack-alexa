f8afcc704b4c070846a088f699fbaaead32ccabe
gsdriver <gsdriver@live.com>
2017-05-16 15:08:20 -0700

    Add cards for rule changes

--- intents/Rules.js
+++ intents/Rules.js
@@ -10,16 +10,12 @@ module.exports = {
   handleIntent: function() {
     playgame.readRules(this.event.session.user.userId,
       (error, response, speech, reprompt, gameState) => {
-      if (gameState) {
-        this.attributes['gameState'] = gameState;
-      }
-
       if (error) {
         this.emit(':ask', error, 'What else can I help with?');
       } else if (response) {
-        this.emit(':tell', response);
+        this.emit(':tellWithCard', response, 'Play Blackjack', response);
       } else {
-        this.emit(':ask', speech, reprompt);
+        this.emit(':askWithCard', speech, reprompt, 'Play Blackjack', speech);
       }
     });
   },
--- intents/Yes.js
+++ intents/Yes.js
@@ -5,22 +5,13 @@
 'use strict';
 
 const playgame = require('../PlayGame');
+const bjUtils = require('../BlackjackUtils');
 
 module.exports = {
   handleIntent: function() {
     playgame.playBlackjackAction(this.event.session.user.userId, 'insurance', 0,
       (error, response, speech, reprompt, gameState) => {
-      if (gameState) {
-        this.attributes['gameState'] = gameState;
-      }
-
-      if (error) {
-        this.emit(':ask', error, 'What else can I help with?');
-      } else if (response) {
-        this.emit(':tell', response);
-      } else {
-        this.emit(':ask', speech, reprompt);
-      }
+      bjUtils.emitResponse(this.emit, error, response, speech, reprompt);
     });
   },
 };
