e2cca723871af1eb21f1a94497139f8aaa2fbca4
gsdriver <gsdriver@live.com>
2017-09-03 18:28:47 -0700

    Wait for service call before sending response

--- BlackjackUtils.js
+++ BlackjackUtils.js
@@ -43,23 +43,28 @@ module.exports = {
         if (err) {
           console.log(err);
         }
+        completed();
       });
-    }
-
-    if (!process.env.NOLOG) {
-      console.log(JSON.stringify(globalEvent));
-    }
-
-    if (error) {
-      const res = require('./' + locale + '/resources');
-      console.log('Speech error: ' + error);
-      emit(':ask', error, res.ERROR_REPROMPT);
-    } else if (response) {
-      emit(':tell', response);
-    } else if (cardTitle) {
-      emit(':askWithCard', speech, reprompt, cardTitle, cardText);
     } else {
-      emit(':ask', speech, reprompt);
+      completed();
+    }
+
+    function completed() {
+      if (!process.env.NOLOG) {
+        console.log(JSON.stringify(globalEvent));
+      }
+
+      if (error) {
+        const res = require('./' + locale + '/resources');
+        console.log('Speech error: ' + error);
+        emit(':ask', error, res.ERROR_REPROMPT);
+      } else if (response) {
+        emit(':tell', response);
+      } else if (cardTitle) {
+        emit(':askWithCard', speech, reprompt, cardTitle, cardText);
+      } else {
+        emit(':ask', speech, reprompt);
+      }
     }
   },
   setEvent: function(event) {
