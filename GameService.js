559fb9cfc2cdbdfac8c407b653581abaff8e7dbe
Garrett Vargas <gsdriver@live.com>
2018-05-27 10:53:25 -0700

    More efficient shuffle

--- GameService.js
+++ GameService.js
@@ -383,6 +383,7 @@ function shuffleDeck(game) {
   // Start by initializing the deck
   let i;
   let rank;
+  const start = Date.now();
 
   game.deck.cards = [];
   const suits = ['C', 'D', 'H', 'S'];
@@ -394,27 +395,21 @@ function shuffleDeck(game) {
     }
   }
 
-  // OK, let's shuffle the deck - we'll do this by going thru
-  // 10 * number of cards times, and swap random pairs each iteration
-  // Yeah, there are probably more elegant solutions but this should do the job
-  for (i = 0; i < game.rules.numberOfDecks * 520; i++) {
-    const randomValue1 = seedrandom(i + game.userID + (game.timestamp ? game.timestamp : ''))();
-    const randomValue2 = seedrandom('A' + i + game.userID + (game.timestamp ? game.timestamp : ''))();
-    const card1 = Math.floor(randomValue1 * game.rules.numberOfDecks * 52);
-    const card2 = Math.floor(randomValue2 * game.rules.numberOfDecks * 52);
-    if (card1 == game.rules.numberOfDecks * 52) {
-      card1--;
+  // Shuffle using the Fisher-Yates algorithm
+  for (i = 0; i < game.deck.cards.length - 1; i++) {
+    const randomValue = seedrandom(i + game.userID + (game.timestamp ? game.timestamp : ''))();
+    let j = Math.floor(randomValue * (game.deck.cards.length - i));
+    if (j == (game.deck.cards.length - i)) {
+      j--;
     }
-    if (card2 == game.rules.numberOfDecks * 52) {
-      card2--;
-    }
-    const tempCard = game.deck.cards[card1];
-
-    game.deck.cards[card1] = game.deck.cards[card2];
-    game.deck.cards[card2] = tempCard;
+    j += i;
+    const tempCard = game.deck.cards[i];
+    game.deck.cards[i] = game.deck.cards[j];
+    game.deck.cards[j] = tempCard;
   }
 
   // Clear out all hands
+  console.log('Shuffle took ' + (Date.now() - start) + ' ms');
   game.activePlayer = 'none';
 }
 
