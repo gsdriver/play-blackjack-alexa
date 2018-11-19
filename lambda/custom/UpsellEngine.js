c6e1a3be749fe9b2deef6dd0637f227384a79e93
Garrett Vargas <gsdriver@live.com>
2018-11-18 17:01:10 -0800

    Upsell after play, not on launch

--- lambda/custom/UpsellEngine.js
+++ lambda/custom/UpsellEngine.js
@@ -9,6 +9,12 @@
 //  3. After getting a 5-card winning 21
 //  4. When asking to select a new game
 //
+// Versions for analysis:
+//  v1.0 (not set) - upsell on launch every 2 days, upsell on long21 after 48 hours,
+//                   always on select
+//  v1.1 - adds sold field, no upsell on launch, upsell on long21 after 24 hours,
+//                   after 6 rounds every two days
+//
 
 'use strict';
 
@@ -31,7 +37,6 @@ module.exports = {
     if (!attributes.upsell) {
       attributes.upsell = {};
       attributes.upsell.prompts = {};
-      attributes.upsell.newUser = true;
     }
     if (!attributes.upsell[trigger]) {
       attributes.upsell[trigger] = {};
@@ -48,6 +53,7 @@ module.exports = {
     // will help us see the full session length
     if (!attributes.upsell.start) {
       attributes.upsell.start = now;
+      attributes.upsell.sessions = (attributes.upsell.sessions + 1) || 1;
     }
 
     attributes.upsell[trigger].trigger = now;
@@ -83,7 +89,8 @@ module.exports = {
       || (attributes.upsell && attributes.upsell.endOnUpsell)) {
       if (response.directives) {
         response.directives.forEach((directive) => {
-          if ((directive.type === 'Connections.SendRequest') && (directive.name === 'Upsell')) {
+          if ((directive.type === 'Connections.SendRequest') &&
+            ((directive.name === 'Upsell') || (directive.name === 'Buy'))) {
             upsell = true;
             attributes.upsell.endOnUpsell = true;
           }
@@ -98,6 +105,8 @@ module.exports = {
           attributes.upsell = {};
         }
         attributes.upsell.end = now;
+        attributes.upsell.sold = (attributes.paid && attributes.paid.spanish
+          && (attributes.paid.spanish.state === 'PURCHASED'));
 
         // Save to S3 - if we are saving data
         if (process.env.SNSTOPIC) {
@@ -112,8 +121,10 @@ module.exports = {
 
         // Clear everything except the prompts data
         const prompts = JSON.parse(JSON.stringify(attributes.upsell.prompts));
+        const sessions = attributes.upsell.sessions;
         attributes.upsell = {};
         attributes.upsell.prompts = prompts;
+        attributes.upsell.sessions = sessions;
         attributes.upsell.lastSession = now;
       }
     }
@@ -135,7 +146,7 @@ function selectUpsellMessage(attributes, message) {
     'LAUNCH_SPANISH_UPSELL': 'Hello, welcome to Blackjack Game. We now have Spanish 21 available for purchase. Want to learn more?|Hi, welcome to Blackjack Game. We\'re proud to introduce a new way to play blackjack with Spanish 21! Want to hear more about it?|Welcome back to Blackjack Game. We have the popular blackjack variant Spanish 21 available for purchase. Want to learn more?',
     'LONG21_SPANISH_UPSELL': 'You got a 21 with five or more cards on your last hand. We have Spanish 21 available for purchase where that pays extra. Would you like to hear more?|Good job getting 21 the hard way. With Spanish 21 that would have paid extra. Would you like to hear more about it?|Getting 21 with that many cards isn\'t easy. In Spanish 21, that pays extra. Are you interested in hearing more about this game?',
     'SELECT_SPANISH_UPSELL': 'You\'ve played standard Blackjack, you can now get the Spanish 21 expansion pack. Want to learn more?|Would you like to hear more about the Spanish 21 expansion pack available for purchase?|We have a Spanish 21 game available for purchase. Want to hear more?',
-    'PLAY_SPANISH_UPSELL': 'You\'ve enjoyed standard Blackjack, you can now get the Spanish 21 expansion pack. Want to learn more?|Would you like to hear more about the Spanish 21 expansion pack available for purchase?|We have a Spanish 21 game available for purchase. Want to hear more?',
+    'PLAY_SPANISH_UPSELL': 'Did you know we have a Spanish 21 expansion pack? Want to learn more?|I\'m glad to see you\'re enjoying Blackjack Game. Would you like to hear about the Spanish 21 expansion pack I have available for purchase?|By the way, we also have a Spanish 21 game available for purchase. Want to hear more?',
     'LISTPURCHASES_SPANISH_UPSELL': 'You don\'t have any products purchased, but we have Spanish 21 available. Want to learn more?|You haven\'t purchased any products, but we have a Spanish 21 expansion pack available for purchase. Would you like to hear more?|You haven\'t bought any products yet, but we have Spanish 21 available for purchase. Want to hear more?',
   };
 
@@ -153,17 +164,11 @@ function shouldUpsell(attributes, trigger, now) {
 
   switch (trigger) {
     case 'launch':
-      // We will trigger if this is not a new user
-      // and we haven't played this upsell in the past 48 hours
-      upsell = (!attributes.upsell.newUser &&
-        (!attributes.upsell.prompts.launch ||
-          ((now - attributes.upsell.prompts.launch) > 2*24*60*60*1000)));
-      attributes.upsell.newUser = undefined;
       break;
 
     case 'long21':
       upsell = (!attributes.upsell.prompts.long21 ||
-        ((now - attributes.upsell.prompts.long21) > 2*24*60*60*1000));
+        ((now - attributes.upsell.prompts.long21) > 24*60*60*1000));
       break;
 
     case 'select':
@@ -172,8 +177,11 @@ function shouldUpsell(attributes, trigger, now) {
       break;
 
     case 'play':
-      // Doesn't currently upsell
-      upsell = false;
+      // Trigger if once they hit 6 hands - once every 2 days and not for the first session
+      upsell = (attributes.upsell.sessions > 1) &&
+        (attributes.upsell.play.count === 6) &&
+        (!attributes.upsell.prompts.play ||
+          ((now - attributes.upsell.prompts.play) > 2*24*60*60*1000));
       break;
 
     case 'listpurchases':
