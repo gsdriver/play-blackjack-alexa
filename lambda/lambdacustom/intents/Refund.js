ea641b0f3194f0e50659b18aeda0a6151743dbd1
Garrett Vargas <gsdriver@live.com>
2022-10-28 07:21:07 -0700

    Refund flow

--- lambda/lambdacustom/intents/Refund.js
+++ lambda/lambdacustom/intents/Refund.js
@@ -9,21 +9,8 @@ const bjUtils = require('../BlackjackUtils');
 module.exports = {
   canHandle: function(handlerInput) {
     const request = handlerInput.requestEnvelope.request;
-    const attributes = handlerInput.attributesManager.getSessionAttributes();
-    let canRefund = false;
 
-    if ((request.type === 'IntentRequest') && (request.intent.name === 'RefundIntent') && attributes.paid) {
-      // Let's see if anything is purchased to refund
-      let product;
-
-      for (product in attributes.paid) {
-        if (product && attributes.paid[product] && (attributes.paid[product].state === 'PURCHASED')) {
-          canRefund = true;
-        }
-      }
-    }
-
-    return canRefund;
+    return (request.type === 'IntentRequest') && (request.intent.name === 'RefundIntent');
   },
   handle: function(handlerInput) {
     const event = handlerInput.requestEnvelope;
@@ -36,26 +23,51 @@ module.exports = {
         .getResponse();
     }
 
-    const product = bjUtils.mapProduct(handlerInput);
-    if (product) {
+    // OK, let's see what needs to be refunded
+    // Or let them know if there is nothing eligible for a refund at this time
+    if (attributes.paid) {
+      // Let's see if anything is purchased to refund
+      let canRefund;
+      let product;
+
+      for (product in attributes.paid) {
+        if (product && attributes.paid[product] && (attributes.paid[product].state === 'PURCHASED')) {
+          canRefund = true;
+        }
+      }
+
+      if (canRefund) {
+        product = bjUtils.mapProduct(handlerInput);
+        if (product) {
+          return handlerInput.responseBuilder
+            .addDirective({
+              'type': 'Connections.SendRequest',
+              'name': 'Cancel',
+              'payload': {
+                'InSkillProduct': {
+                  'productId': attributes.paid[product].productId,
+                },
+              },
+              'token': 'game.' + product + '.launch',
+            })
+            .withShouldEndSession(true)
+            .getResponse();
+        } 
+      }
+    }
+
+    // If they bought a good, then we should direct them to Amazon to cancel the purchase
+    if (attributes.purchasedGoods && Object.keys(attributes.purchasedGoods).length) {
       return handlerInput.responseBuilder
-        .addDirective({
-          'type': 'Connections.SendRequest',
-          'name': 'Cancel',
-          'payload': {
-            'InSkillProduct': {
-              'productId': attributes.paid[product].productId,
-            },
-          },
-          'token': 'game.' + product + '.launch',
-        })
+        .speak(res.strings.BUY_GOOD_CANCEL)
         .withShouldEndSession(true)
         .getResponse();
-    } else {
-      return handlerInput.responseBuilder
+    }
+
+    // Guess there's nothing to refund
+    return handlerInput.responseBuilder
         .speak(res.strings.PURCHASE_NO_PURCHASE)
         .reprompt(res.strings.PURCHASE_REPROMPT)
         .getResponse();
-    }
   },
 };
